cb5b63de86fe15781d73bf17205d65c5137ce335
Tyler <vick.tyler@gmail.com>
2020-06-09 16:07:07 -0400

    Add item with a modal

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { FlatList, StyleSheet, View, Button } from 'react-native';
 import PlantInput from './components/PlantInput';
 import PlantItem from './components/PlantItem';
 
 export default function App() {
   const [plants, setPlants] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   function addPlant(plantTitle) {
     setPlants(currentPlants => [
@@ -14,6 +15,8 @@ export default function App() {
         value: plantTitle,
       }
     ])
+
+    setIsModalOpen(false);
   }
 
   function removePlant(plantId) {
@@ -22,8 +25,14 @@ export default function App() {
 
   return (
     <View style={styles.screen}>
+      <Button 
+        title="Add New Plant"
+        onPress={() => setIsModalOpen(true)}
+      />
       <PlantInput
+        isVisible={isModalOpen}
         onAddPlant={addPlant}
+        onCancel={() => setIsModalOpen(false)}
       />
       <FlatList
         keyExtractor={(item) => item.id}
--- components/PlantInput.js
+++ components/PlantInput.js
@@ -1,32 +1,58 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View } from 'react-native';
 
 function PlantInput(props) {
   const [enteredPlant, setEnteredPlant] = useState('');
 
-  function inputChangeHandler(enteredText) {
+  function handleInputChangeText(enteredText) {
     setEnteredPlant(enteredText);
   }
 
-  return (<View style={styles.inputContainer}>
-    <TextInput
-      placeholder='Plant'
-      style={styles.input}
-      onChangeText={inputChangeHandler}
-      value={enteredPlant}
-    />
-    <Button
-      title="ADD"
-      style={{}}
-      onPress={() => props.onAddPlant(enteredPlant)}
-    />
-  </View>);
+  function handleAddButtonPress() {
+    props.onAddPlant(enteredPlant);
+    setEnteredPlant('');
+  }
+
+  function handleCancelButtonPress() {
+    props.onCancel();
+  }
+
+  return (
+    <Modal
+      visible={props.isVisible}
+      animationType="slide"
+    >
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder='Plant'
+          style={styles.input}
+          onChangeText={handleInputChangeText}
+          value={enteredPlant}
+        />
+        <View style={styles.buttonPair}>
+          <View style={styles.button}>
+            <Button
+              title="CANCEL"
+              color="gray"
+              onPress={handleCancelButtonPress}
+            />
+          </View>
+          <View style={styles.button}>
+            <Button
+              title="ADD"
+              onPress={handleAddButtonPress}
+            />
+          </View>
+        </View>
+      </View>
+    </Modal>
+  );
 }
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   input: {
@@ -34,6 +60,15 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     width: '80%',
+    marginBottom: 10,
+  },
+  buttonPair: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '80%',
+  },
+  button: {
+    width: '40%',
   }
 });
 
